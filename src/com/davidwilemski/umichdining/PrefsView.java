1fc5f4bbe1dce33bd0b83f5163d183137e886fab
Bryan Kendall <bryan.a.kendall@gmail.com>
2010-12-28 00:18:42 -0500

    Got Twitter started to be implimented. Getting an error while trying to get user tokens though.

--- src/com/davidwilemski/umichdining/PrefsView.java
+++ src/com/davidwilemski/umichdining/PrefsView.java
@@ -1,5 +1,20 @@
 package com.davidwilemski.umichdining;
 
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.exception.OAuthNotAuthorizedException;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -7,6 +22,20 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.widget.Toast;
 
 public class PrefsView extends PreferenceActivity {
+	public static final String PREFS_NAME = "UmichPrefFile";
+	public static final String CONSUMER_KEY = "TGy9UGMp5t2WbJQ8ThWYWQ";
+	public static final String CONSUMER_SECRET = "omL1ZoIF2ehXv6yXbvwov5DuexG5bxgNbye62BNIB0w";
+	public static final String CALLBACK_URL = "umichdining://twitter";
+	
+	CommonsHttpOAuthConsumer consumer = new CommonsHttpOAuthConsumer(  
+	        CONSUMER_KEY, CONSUMER_SECRET);  
+	  
+	OAuthProvider provider = new CommonsHttpOAuthProvider(
+			"http://twitter.com/oauth/request_token", 
+			"http://twitter.com/oauth/access_token",  
+	        "http://twitter.com/oauth/authorize");  
+	
+	HttpClient client = new DefaultHttpClient(); 
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,9 +44,61 @@ public class PrefsView extends PreferenceActivity {
 		Preference twitter_pref = findPreference("TWITTER");
 		twitter_pref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 			public boolean onPreferenceClick(Preference preference) {
-				Toast.makeText(getBaseContext(), "Changed the Twitter pref.", Toast.LENGTH_SHORT).show();
+				SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+				SharedPreferences.Editor editor = settings.edit();
+				boolean new_setting = !settings.getBoolean("TWITTER", false);
+				if(new_setting) {
+					// We are turning twitter on...
+					Toast.makeText(getApplicationContext(), "Turning on Twitter", Toast.LENGTH_SHORT).show();
+					String authUrl = null;
+					try {
+						authUrl = provider.retrieveRequestToken(consumer, CALLBACK_URL);
+					} catch (OAuthMessageSignerException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (OAuthNotAuthorizedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (OAuthExpectationFailedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (OAuthCommunicationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}  
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl))); 
+				} else {
+					// We are turning twitter off...
+					Toast.makeText(getApplicationContext(), "Turning off Twitter", Toast.LENGTH_SHORT).show();
+					editor.putBoolean("TWITTER", false);
+				}
 				return true;
 			}
 		});
+	}
+	
+	protected void onResume() {
+		super.onResume();
+		Uri uri = this.getIntent().getData();  
+		if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {  
+		    String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);  
+		    // this will populate token and token_secret in consumer  
+		    try {
+				provider.retrieveAccessToken(consumer, verifier);
+			} catch (OAuthMessageSignerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (OAuthNotAuthorizedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (OAuthExpectationFailedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (OAuthCommunicationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("TOKEN: " + consumer.getToken());
+		} 
 	}
 }
